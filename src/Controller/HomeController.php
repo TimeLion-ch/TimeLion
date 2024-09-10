e0751c59a710fd250fe64f4ccbee4d5e168a14d7
c14ud3 <work@fleisch-mann.ch>
2024-09-10 22:58:01 +0200

    CSS/HTML, Base Navbar and Language Handler

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'Hello World!',
-        ]);
-    }
+	#[Route('/', name: 'home')]
+	public function index(): Response
+	{
+		return $this->render('home/index.html.twig');
+	}
+
+	#[Route('/lang/{language}', name: 'set_language')]
+	public function language($language, Request $request): Response
+	{
+		$request->getSession()->set('_locale', $language);
+		return $this->redirectToRoute('home');
+	}
 }
